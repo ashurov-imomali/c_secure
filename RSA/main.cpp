f721e0c5178c6f5378c40a1cdfc2e4877a8a0333
imomali <lashurov.l000@gmail.com>
2025-03-07 13:41:00 +0500

    add interface

--- RSA/main.cpp
+++ RSA/main.cpp
@@ -3,7 +3,7 @@
 #include <cmath>
 #include <locale>
 #include <codecvt>
-
+#include <windows.h>
 
 std::string readFile(std::string fileName){
     std::ifstream file(fileName);
@@ -61,7 +61,7 @@ void writeToFile(std::string stream, std::string fileName) {
     f.close();
 }
 
-std::string Decode(std::string fileName, int p, int q, int d){
+std::string Decode(std::string fileName, int p, int q, int d, std::string fileToWrite){
     std::string utf8str = readFile(fileName);
     std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> convert;
     std::u32string file = convert.from_bytes(utf8str);
@@ -73,12 +73,12 @@ std::string Decode(std::string fileName, int p, int q, int d){
     }
 
     std::string utf8res = convert.to_bytes(res32);
-    writeToFile(utf8str, "../output.txt");
+    writeToFile(utf8res, fileToWrite);
     return utf8res;
 }
 
 
-std::string Encode(const std::string& fileName, int p, int q, int e) {
+std::string Encode(const std::string& fileName, int p, int q, int e, std::string fileToWrite) {
     std::string utf8Input = readFile(fileName);
     if (utf8Input.empty()) {
         return {};
@@ -99,7 +99,7 @@ std::string Encode(const std::string& fileName, int p, int q, int e) {
     }
 
     std::string encryptedUtf8 = convert.to_bytes(u32Output);
-    writeToFile(encryptedUtf8, "../input.txt");
+    writeToFile(encryptedUtf8, fileToWrite);
     return encryptedUtf8;
 }
 
@@ -148,34 +148,34 @@ void Interface()
         std::cin >> nc;
         if (nc == 1)
         {
-            std::cout << "P Q D: ";
+            std::cout << "P Q D (73 103 941):";
             int p, q, d;
             std::cin >> p >> q >> d;
-            std::cout << Encode("../input.txt", p, q,d) << "\nSave to input.txt\n\n";
+            std::cout << Encode("../input.txt", p, q,d, "../output.txt") << "\nSave to output.txt\n\n";
         }else
         {
-                std::cout << "P Q D: ";
+                std::cout << "P Q D (73 103 941):";
                 int p, q, d;
                 std::cin >> p >> q >> d;
-                std::cout << Encode("../output.txt", p, q,d) << "\nSave to output.txt\n\n";
+                std::cout << Encode("../output.txt", p, q,d, "../input.txt") << "\nSave to input.txt\n\n";
         }
         Interface();
     case 2:
-        std::cout << "Choose file\n 1.input.txt\n2.output.txt";
+        std::cout << "Choose file\n1.input.txt\n2.output.txt\n";
         int ncc;
         std::cin >> ncc;
         if (ncc == 1)
         {
-            std::cout << "P Q D: ";
+            std::cout << "P Q D (73 103 941):";
             int p, q, d;
             std::cin >> p >> q >> d;
-            std::cout << Decode("../input.txt", p, q,d) << "\nSave to input.txt\n\n";
+            std::cout << Decode("../input.txt", p, q,d, "../output.txt") << "\nSave to output.txt\n\n";
         }else
         {
-            std::cout << "P Q D: ";
+            std::cout << "P Q D (73 103 941):";
             int p, q, d;
             std::cin >> p >> q >> d;
-            std::cout << Decode("../output.txt", p, q,d) << "\nSave to output.txt\n\n";
+            std::cout << Decode("../output.txt", p, q,d, "../input.txt") << "\nSave to input.txt\n\n";
         }
         Interface();
     default:
@@ -185,46 +185,51 @@ void Interface()
 
 
 int main(){
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
     Interface();
     int p = 73, q = 103, d = 941;
-    std::cout << Encode("../output.txt", p,q, findPublicExponent(p,q,d));
+
+    // std::cout << Decode("../input.txt", p,q,d);
     return 0;
-    std::cout << Decode("../input.txt", 73, 103, 941) << "\n\n";
-    int e = findPublicExponent(p,q,d);
-    std::cout << Encode("../output.txt", p, q, e) << "\n\n";
-    std::cout << Decode("../input1.txt",p,q,d);
-    return 0;
-    std::string fileName = "../input.txt";
-    std::string u32txt = readFile(fileName);
-    std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t>  convert;
-    std::u32string file = convert.from_bytes(u32txt);
-    int k = 0, counter = 0;
-    int ps[100], ds[100], qs[100];
-    for (int p = 11; p < 101; p+=2) {
-        if (!isPrime(p)) continue;
-        for (int q = 101; q < 1000; q+=2) {
-            if (!isPrime(q)) continue;
-            for (int d = 10; d < p*q+1; ++d) {
-                if (checkA(modExp(file[k],d,p*q)) && checkA(modExp(file[k+1],d,p*q)) && checkA(modExp(file[k+2],d,p*q))){
-                    std::cout << "P: " << p << " Q: " << q << " D: " << d << "\n\n";
-                    ps[counter] = p;
-                    qs[counter] = q;
-                    ds[counter++] = d;
-                }
-            }
-        }
-    }
-    for (int j = 0; j < counter; ++j) {
-        std::u32string res32;
-        res32.reserve(file.size());
-        for (int i = 0; i < file.length(); ++i) {
-            auto codePoint = modExp(file[i], ds[j], ps[j]*qs[j]);
-            res32.push_back(static_cast<char32_t>(codePoint));
-        }
-        std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> convert;
-        std::string utf8res = convert.to_bytes(res32);
-        std::cout << "p: " << ps[j] << " q: " << qs[j] << " d: " << ds[j] << "\n" << utf8res << "\n\n";
-    }
+    // std::cout << Decode("../input.txt", 73, 103, 941) << "\n\n";
+    // int e = findPublicExponent(p,q,d);
+    // std::cout << Encode("../output.txt", p, q, e) << "\n\n";
+    // std::cout << Decode("../input1.txt",p,q,d);
+    // return 0;
+    // std::string fileName = "../input.txt";
+    // std::string u32txt = readFile(fileName);
+    // std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t>  convert1;
+    // std::u32string file = convert1.from_bytes(u32txt);
+    // int k = 0, counter = 0;
+    // int ps[100], ds[100], qs[100];
+    // for (int p = 11; p < 101; p+=2) {
+    //     if (!isPrime(p)) continue;
+    //     for (int q = 101; q < 1000; q+=2) {
+    //         if (!isPrime(q)) continue;
+    //         for (int d = 10; d < p*q+1; ++d) {
+    //             if (checkA(modExp(file[k],d,p*q)) && checkA(modExp(file[k+1],d,p*q)) && checkA(modExp(file[k+2],d,p*q))){
+    //                 std::cout << "P: " << p << " Q: " << q << " D: " << d << "\n\n";
+    //                 ps[counter] = p;
+    //                 qs[counter] = q;
+    //                 ds[counter++] = d;
+    //             }
+    //         }
+    //     }
+    // }
+    // for (int j = 0; j < counter; ++j) {
+    //     std::u32string res32;
+    //     res32.reserve(file.size());
+    //     for (int i = 0; i < file.length(); ++i) {
+    //         auto codePoint = modExp(file[i], ds[j], ps[j]*qs[j]);
+    //         res32.push_back(static_cast<char32_t>(codePoint));
+    //     }
+    //     std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> convert;
+    //     std::string utf8res = convert.to_bytes(res32);
+    //     SetConsoleOutputCP(CP_UTF8);
+    //     SetConsoleCP(CP_UTF8);
+    //     std::cout << "p: " << ps[j] << " q: " << qs[j] << " d: " << ds[j] << "\n" << utf8res << "\n\n";
+    // }
 
 
 }
